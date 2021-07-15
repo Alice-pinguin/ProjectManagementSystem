693eec689da39ed9fc41977a1e56946d19ae0afc
alice_just <alicejust53@gmail.com>
2021-07-15 15:01:37 +0300

    Create controller and util

---
+++ src/main/java/controller/DataBaseConnection.java
@@ -0,0 +1,63 @@
+package controller;
+
+import lombok.SneakyThrows;
+import utils.PropertiesLoader;
+
+import java.io.Closeable;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection  implements Closeable {
+
+    private static final String user_name = PropertiesLoader.getProperty("name");
+    private static final String password = PropertiesLoader.getProperty("password");
+    private static final String URL = PropertiesLoader.getProperty("url");
+    private static final String JDBC_DRIVER = PropertiesLoader.getProperty("driver");
+    private static DataBaseConnection dataBaseConnection;
+    private  Connection connection;
+
+
+    public DataBaseConnection()throws SQLException {
+        try {
+            Class.forName(JDBC_DRIVER);
+        }  catch (ClassNotFoundException e) {
+            throw new ClassCastException("No data found");
+        }
+        this.connection = DriverManager.getConnection(URL, user_name, password);
+    }
+
+
+    private DataBaseConnection getInstance() throws SQLException {
+        if (dataBaseConnection == null || getConnection().isClosed()) {
+            dataBaseConnection = new DataBaseConnection();
+        }
+        return dataBaseConnection;
+    }
+
+    private  Connection getConnection() {
+       return connection;
+    }
+
+
+
+
+    @Override
+    public void close() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+           throw new RuntimeException("Connection doesn't closed");
+        }
+
+    }
+
+    public static void main(String[] args) throws SQLException {
+        DataBaseConnection dataBaseConnection = new DataBaseConnection();
+        System.out.println(dataBaseConnection);
+    }
+}
+
+
+
---
+++ src/main/java/utils/PropertiesLoader.java
@@ -0,0 +1,30 @@
+package utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesLoader {
+
+    private static final String properties_name = "db.properties";
+
+    private static final Properties properties;
+
+    static {
+        properties = new Properties();
+        try (InputStream inputStream = getInputStream(properties_name)) {
+            properties.load(inputStream);
+        } catch (IOException ex) {
+            throw new RuntimeException("Invalid information");
+        }
+    }
+
+    public static String getProperty (String name) {
+        return properties.getProperty(name);
+    }
+
+    public static InputStream getInputStream(String propertiesFileName) {
+        return PropertiesLoader.class.getClassLoader().getResourceAsStream(propertiesFileName);
+    }
+
+}
