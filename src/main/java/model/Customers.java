1d682646cc49abac1674cc7c74e8c967854816aa
alice_just <alicejust53@gmail.com>
2021-07-27 14:46:35 +0300

    Update package model

--- src/main/java/model/Customers.java
+++ src/main/java/model/Customers.java
@@ -5,16 +5,29 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serial;
+import java.io.Serializable;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Customers implements BaseEntity<Long> {
+@Table (name = "customers")
 
-        private String name;
+public class Customers implements BaseEntity<Long>, Serializable {
+
+        @Serial
+        private static final long serialVersionUID = 5612028522264576789L;
+        @Id
         private Long id;
+        @Column (name = "Name")
+        private String name;
+        @Column (name = "City")
         private String city;
+        @Column (name = "Industry")
         private String industry;
 
 }
