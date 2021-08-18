308bac3cd630df57ac7ded47549113ff3ca6325d
Алиса Кондратенко <78209371+Alice-pinguin@users.noreply.github.com>
2021-08-18 22:14:56 +0300

    Delete src/main/java/ua/goit/service directory

--- src/main/java/ua/goit/service/CompanyProjectServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package ua.goit.service;
-
-
-import ua.goit.model.CompaniesProjects;
-
-public class CompanyProjectServiceImpl extends BaseService<CompaniesProjects, Long> {
-
-}
--- src/main/java/ua/goit/service/CompanyServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package ua.goit.service;
-
-import ua.goit.model.Companies;
-
-
-public  class CompanyServiceImpl extends BaseService<Companies, Long> {
-
-}
-
