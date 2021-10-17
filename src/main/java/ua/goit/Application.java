6654bc7688310b788859a52bd247e2a22d1e5c77
alice_just <alicejust53@gmail.com>
2021-10-17 14:21:16 +0300

    Update service query, create dto project

--- src/main/java/ua/goit/Application.java
+++ src/main/java/ua/goit/Application.java
@@ -14,13 +14,13 @@ public class Application {
    @SneakyThrows
     public static void main(String[] args) {
 //        ScriptExecutor.start();
-//         ConsoleController consoleController = new ConsoleController (new Console ());
-//consoleController.process ();
+         ConsoleController consoleController = new ConsoleController (new Console ());
+consoleController.process ();
 
-       QueryExecutorImpl queryExecutor = new QueryExecutorImpl ();
-       System.out.println (queryExecutor.getDeveloperByLevel ("Senior"));
-       System.out.println (queryExecutor.getDeveloperByLevel ("Middle"));
-       System.out.println (queryExecutor.getDevelopersBySkill ("Java"));
+//       QueryExecutorImpl queryExecutor = new QueryExecutorImpl ();
+//       System.out.println (queryExecutor.getDeveloperByLevel ("Senior"));
+//       System.out.println (queryExecutor.getDeveloperByLevel ("Middle"));
+//       System.out.println (queryExecutor.getDevelopersBySkill ("Java"));
    }
     }
 
